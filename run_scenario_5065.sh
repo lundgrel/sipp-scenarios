f86bd450fb8bb59762046ab31e02afff5a595704
Leif Lundgren, Mitel <leif.lundgren@mitel.com>
2019-04-25 14:11:21 +0000

    got scenario working

--- run_scenario_5065.sh
+++ run_scenario_5065.sh
@@ -1,1 +1,3 @@
-sipp 10.105.72.24:5065 -sf "$1" -m 1 -trace_msg -i 10.105.72.24 -p 5432
+basename=${1%.*}
+rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
+../sipp/sipp 10.105.72.24:5065 -sf "$1" -m 1 -trace_msg -trace_err -p 5432
