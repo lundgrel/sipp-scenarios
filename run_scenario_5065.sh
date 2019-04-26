4f3489fc6e798ae1bbd1b018bac9a6c0321aa83c
Leif Lundgren <leif.lundgren@mitel.com>
2019-04-26 12:55:42 +0200

    Managed to test streaming pcap to media server

--- run_scenario_5065.sh
+++ run_scenario_5065.sh
@@ -1,3 +1,3 @@
 basename=${1%.*}
 rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
-../sipp/sipp 10.105.72.24:5065 -sf "$1" -m 1 -trace_msg -trace_err -p 5432
+../sipp/sipp 10.105.72.24:5065 -sf "$1" -s 888 -m 1 -trace_msg -trace_err -p 5432
