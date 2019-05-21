f5dd0f8ecac40df51d27a5bb2a1b4e79c78b1ab7
Leif Lundgren <leif.lundgren@mitel.com>
2019-05-21 16:00:21 +0200

    Work on getting streaming PCAP to work!

--- run_scenario_5065.sh
+++ run_scenario_5065.sh
@@ -1,3 +1,7 @@
+#!/bin/sh -x
+
 basename=${1%.*}
 rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
-../sipp/sipp 10.105.72.24:5065 -sf "$1" -s 888 -m 1 -trace_msg -trace_err -p 5432
+../sipp/sipp.git/sipp 10.105.72.24:5065 -sf "$1" -s 888 -l 1 -t t1 -aa -trace_msg -trace_err -mp 7206 -p 4268
+echo completed
+cat ${basename}_[0-9]*_errors.log
