1ce95147c644e30855f43ffe78bddd6e76dabd9e
Leif Lundgren <leif.lundgren@mitel.com>
2019-08-14 12:06:54 +0200

    Added scenarios making a call with a=inactive.

--- run_scenario_5065.sh
+++ run_scenario_5065.sh
@@ -2,6 +2,6 @@
 
 basename=${1%.*}
 rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
-../sipp/sipp.git/sipp 10.105.72.24:5065 -sf "$1" -s 888 -l 1 -t t1 -aa -trace_msg -trace_err -mp 7206 -p 4268
+../sipp/sipp.git/sipp 10.105.72.24:5065 -sf "$1" -i 10.105.79.90 -s 888 -l 1 -aa -trace_msg -trace_err -mp 7206 -p 4268 -rate_increase 37 -rate_max 1 -m 1 
 echo completed
 cat ${basename}_[0-9]*_errors.log
