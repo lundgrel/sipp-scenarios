5bb8adb18bb348015ed9a0dcd036466eff2b89ff
Leif Lundgren <leif.lundgren@mitel.com>
2019-10-09 14:40:01 +0200

    Some added scenarios

--- run_scenario_5060.sh
+++ run_scenario_5060.sh
@@ -2,6 +2,6 @@
 
 basename=${1%.*}
 rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
-../sipp/sipp.git/sipp 10.105.72.24:5060 -sf "$1" -s 888 -l 1 -m 1 -trace_msg -trace_err -mp 8000
+../sipp/sipp.git/sipp 10.105.72.24:5060 -sf "$1" -s 90019 -l 1 -m 1 -trace_msg -trace_err -mp 8000
 echo completed
 cat ${basename}_[0-9]*_errors.log
