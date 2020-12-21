bcb3f58feffdd062d2781c3a64c569084c4b5ef6
root <leif.lundgren@mitel.com>
2020-12-21 12:25:58 +0000

    Misc

--- run_scenario_5060.sh
+++ run_scenario_5060.sh
@@ -4,7 +4,7 @@ basename=${1%.*}
 rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
 DN="${2:-90019}"
 echo run sipp
-../sipp/sipp.git/sipp 10.105.72.24:5060 -sf "$1" -s "$DN" -l 1 -m 1 -trace_msg -trace_err -mp 8000 -p 5060 -t t1
+../sipp/sipp.git/sipp 10.105.72.24:5060 -sf "$1" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -mp 8000 -p 5060 -t t1
 echo completed
 cat ${basename}_[0-9]*_errors.log
 echo .
--- run_uac_to_devstation.sh
+++ run_uac_to_devstation.sh
@@ -4,6 +4,6 @@ basename=${1%.*}
 rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
 DN="${2:-90019}"
 echo run sipp
-../sipp/sipp.git/sipp 10.105.72.24:5060 -sf "$1" -s "$DN" -l 1 -m 1 -trace_msg -trace_err -mp 8000  -t t1
+../sipp/sipp.git/sipp 10.105.79.12:5062 -sf "$1" -s "$DN" -l 1 -m 1 -trace_msg -trace_err -mp 8000  -t t1
 echo completed
 cat ${basename}_[0-9]*_errors.log
