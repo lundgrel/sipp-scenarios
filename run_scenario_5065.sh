deb4544aa050d069b2f99d9bffe422a9874972a2
root <leif.lundgren@mitel.com>
2021-03-15 16:06:10 +0000

    misc work

--- run_scenario_5065.sh
+++ run_scenario_5065.sh
@@ -3,7 +3,7 @@
 echo I am running as `id`
 basename=${1%.*}
 rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
-../sipp/sipp.git/sipp 10.105.72.24:5065 -sf "$1" -i 10.105.79.90 -s 888 -l 1 -aa -trace_msg -trace_err -mp 7206 -p 4268 -rate_increase 37 -rate_max 1 -m 1 -message_file ${basename}_messages.log -error_file ${basename}_errors.log
+../sipp/sipp.git/sipp 10.105.79.12:5065 -sf "$1" -i 10.105.79.90 -s 888 -l 1 -aa -trace_msg -trace_err -mp 7206 -p 4268 -rate_increase 37 -rate_max 1 -m 1 -message_file ${basename}_messages.log -error_file ${basename}_errors.log
 echo completed
 cat ${basename}_errors.log
 echo .
---
+++ run_scenario_5065_pyrite.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo I am running as `id`
+basename=${1%.*}
+rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
+../sipp/sipp.git/sipp 10.8.74.183:5065 -sf "$1" -i 10.105.79.90 -s 888 -l 1 -aa -trace_msg -trace_err -mp 7206 -p 4268 -rate_increase 37 -rate_max 1 -m 1 -message_file ${basename}_messages.log -error_file ${basename}_errors.log
+echo completed
+cat ${basename}_errors.log
+echo .
