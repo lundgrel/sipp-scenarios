da68f9ffc0b0400b257e3e5afb5ded4bb7a362f1
root <root@host>
2023-01-27 14:07:10 +0000

    more changes

--- run_scenario_5062.sh
+++ run_scenario_5062.sh
@@ -2,9 +2,10 @@
 
 basename=${1%.*}
 rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
-DN="${2:-90019}"
+DN="${2:-2012}"
 echo run sipp
-../sipp/sipp.git/sipp 10.105.79.12:5062 -sf "$1" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -mp 8000 -p 5060 -t t1
+# -mp 8000   media-port
+../sipp.git/sipp 10.105.79.12:5062 -sf "$1" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -p 5060 -t t1
 echo completed
 cat ${basename}_[0-9]*_errors.log
 echo .
