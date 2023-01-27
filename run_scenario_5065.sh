8b86343e269205bb5e393d3a72e99c48d1f8e71f
root <root@host>
2023-01-27 14:05:31 +0000

    Updated schenarios

--- run_scenario_5065.sh
+++ run_scenario_5065.sh
@@ -2,8 +2,9 @@
 
 echo I am running as `id`
 basename=${1%.*}
-rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
-../sipp/sipp.git/sipp 10.105.79.12:5065 -sf "$1" -i 10.105.79.90 -s 888 -t t1 -l 1 -aa -trace_msg -trace_err -mp 7206 -p 4268 -rate_increase 37 -rate_max 1 -m 1 -message_file ${basename}_messages.log -error_file ${basename}_errors.log
+rm -v ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log 2> /dev/null && true
+
+../sipp.git/sipp 10.105.79.12:5065 -sf "$1" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -p 5060 -t t1
 echo completed
 cat ${basename}_errors.log
 echo .
