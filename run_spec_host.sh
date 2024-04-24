c0bc97e3ca7ab9a7fff0a85ba38db2b1a305a4b6
Leif Lundgren <leif.lundgren@mitel.com>
2024-04-24 13:11:52 +0000

    mhhmmm

--- run_spec_host.sh
+++ run_spec_host.sh
@@ -11,7 +11,11 @@ pyrite_ip=$2
 
 rm -vf ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
 
-../sipp.git/sipp.root ${pyrite_ip} -sf "$1" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -p 5060 -t t1 -message_file ${basename}_messages.log -error_file ${basename}_errors.log
+echo ../sipp.git/sipp.root ${pyrite_ip} -sf "$1" -s "$DN" -l 1 -aa -trace_msg -trace_err -p 5060 -t t1 -message_file ${basename}_messages.log -error_file ${basename}_errors.log > ${basename}_messages.log 
+../sipp.git/sipp.root ${pyrite_ip} -sf "$1" -s "$DN" -l 1 -aa -trace_msg -trace_err -p 5060 -t t1 -message_file ${basename}_messages.log -error_file ${basename}_errors.log
 echo completed
+echo ../sipp.git/sipp.root ${pyrite_ip} -sf "$1" -s "$DN" -l 1 -aa -trace_msg -trace_err -p 5060 -t t1 -message_file ${basename}_messages.log -error_file ${basename}_errors.log >> ${basename}_messages.log 
+
 cat ${basename}_errors.log
+cat ${basename}_messages.log
 echo .
