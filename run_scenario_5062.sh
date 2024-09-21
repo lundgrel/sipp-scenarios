50ab4a48e3d0b7d805f6e09ac21cee10a2a7cc2a
Leif Lundgren <leif.lundgren@mitel.com>
2024-09-21 21:58:56 +0000

    more tests

--- run_scenario_5062.sh
+++ run_scenario_5062.sh
@@ -6,7 +6,7 @@ DN="${2:-2012}"
 echo run sipp
 # -mp 8000   media-port
 # -p 5060  local port
-../sipp.git/sipp.root 10.105.79.12:5062 -sf "$1" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -t u1 
+../sipp.git/sipp.root 10.105.79.12:5062 -sf "$1" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -t t1 
 echo completed
 cat ${basename}_[0-9]*_errors.log
 echo .
