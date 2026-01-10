daafef519f4be03130f4dbc04e47e7d8ce7b6e34
Leif Lundgren <leif.lundgren@mitel.com>
2026-01-10 20:22:36 +0100

    various work

--- run_scenario_5062.sh
+++ run_scenario_5062.sh
@@ -1,16 +1,17 @@
-#!/bin/sh -x
+#!/bin/bash
 
 basename=${1%.*}
 rm -vf ${basename}_[0-9]*_messages.log ${basename}_[0-9]*_errors.log
-DN="${2:-2012}"
+DN="${2:-90010}"
 
+ss state time-wait dport = 5062
 ss state time-wait dport = 5062 -K
 
 
 echo run sipp
 # -mp 8000   media-port
 # -p 5060  local port
-../sipp.git/sipp.root 10.105.79.12:5062 -sf "$1" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -t t1 
+../sipp.git/sipp.root 10.105.79.12:5062 -sf "$1" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -t u1 
 echo completed
 cat ${basename}_[0-9]*_errors.log
 echo .
