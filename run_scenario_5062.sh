786c5bcd3e2f83d045e407445f813472afe75ba7
Leif Lundgren <leif.lundgren@mitel.com>
2026-06-11 18:27:38 +0000

    mod script

--- run_scenario_5062.sh
+++ run_scenario_5062.sh
@@ -12,7 +12,8 @@ ss state time-wait dport = 5062 -K
 echo run sipp
 # -mp 8000   media-port
 # -p 5060  local port
-../sipp.git/sipp.root 10.105.79.12:5062 -sf "$1" -oocsf "$OOC_SCENARIO" -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -t u1 
+../sipp.git/sipp.root 10.105.79.12:5062 -sf "$1"  -oocsf "$OOC_SCENARIO"  -s "$DN" -l 1 -m 1 -aa -trace_msg -trace_err -t t1 
 echo completed
 cat ${basename}_[0-9]*_errors.log
 echo .
+
